600ed743e9d55ba96f5cf756cc13a0fbd2812db6
eyepax_MenakaS <menaka.sirisena@eyepax.com>
2021-02-21 09:34:00 +0530

    modifications to match with old tutorial

---
+++ Web/App_Start/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Web.App_Start.Startup))]
+
+namespace Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap
+                = new Dictionary<string, string>();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                ClientId = "socialnetwork_implicit",
+                Authority = "http://localhost:44335",
+                RedirectUri = "http://localhost:57919/",
+                ResponseType = OpenIdConnectResponseType.Token,
+                Scope = "openid profile",
+                SignInAsAuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            });
+        }
+    }
+}
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -15,7 +15,6 @@ namespace Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
